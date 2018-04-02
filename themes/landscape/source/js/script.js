214f6b22dd5e6470de2022a70f37a907688ab616
Jono Cooper <jono@jonocooper.com>
2018-04-02 10:49:55 +0000

    Fix Turbolinks Bugs

--- themes/landscape/source/js/script.js
+++ themes/landscape/source/js/script.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+	var nav = document.getElementById('navigation')
 	document.getElementById('expand').addEventListener('click', function() {
-		var nav = document.getElementById('navigation')
 		if (nav.className === '') {
 			nav.className = 'visible'
 		} else {
 			nav.className = ''
 		}
+	})
+	document.querySelectorAll('#navigation li').forEach(function(item) {
+		item.addEventListener('click', function() {
+			nav.className = ''
+		})
 	})
 })
