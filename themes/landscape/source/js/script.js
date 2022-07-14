2538a25cfd37c5b84358e1570aa72e5195325288
Jono Cooper <jono@jonocooper.com>
2022-07-14 22:16:57 +1200

    redesign site

--- themes/landscape/source/js/script.js
+++ themes/landscape/source/js/script.js
@@ -1,15 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-	var nav = document.getElementById('navigation')
-	document.getElementById('expand').addEventListener('click', function() {
-		if (nav.className === '') {
-			nav.className = 'visible'
-		} else {
-			nav.className = ''
-		}
-	})
-	document.querySelectorAll('#navigation li').forEach(function(item) {
-		item.addEventListener('click', function() {
-			nav.className = ''
-		})
-	})
-})
+// no js! you will need to enable this in head.ejs
